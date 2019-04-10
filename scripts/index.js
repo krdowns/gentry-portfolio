5341a2f9fcb44ec8a3b58e133633c9aa00f53f3a
krdowns <krjdowns16@gmail.com>
2019-04-10 10:57:51 -0700

    updated

--- scripts/index.js
+++ scripts/index.js
@@ -19,9 +19,8 @@ window.onload = function(){
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
-    }
-
 };
